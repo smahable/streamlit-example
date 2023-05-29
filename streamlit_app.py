97da6c27f1c81f0348a9558bf816cf5ab3080085
sm2022 <96354662+smahable@users.noreply.github.com>
2023-05-29 12:57:04 -0500

    Update streamlit_app.py
    fn

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,7 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
-import re
+import yfinance as yf
 
-collect_numbers = lambda x : [int(i) for i in re.split("[^0-9]", x) if i != ""]
-
-numbers = st.text_input("PLease enter numbers")
-st.write(collect_numbers(numbers))
-
-fixed_numbers = st.multiselect("Please select numbers", [1, 2, 3, 4, 5])
-st.write(fixed_numbers)
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-collect_numbers = lambda x : [int(i) for i in re.split("[^0-9]", x) if i != ""]
-
-numbers = st.text_input("PLease enter numbers")
-st.write(collect_numbers(numbers))
-
-fixed_numbers = st.multiselect("Please select numbers", [1, 2, 3, 4, 5])
-st.write(fixed_numbers)
+st.title("Stocks App")
+symbol = st.text_input("Enter a stock symbol", "AAPL")
+if st.button("Get Quote"):
+    st.json(yf.Ticker(symbol).info)
